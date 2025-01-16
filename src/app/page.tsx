7ab94b8ae16b80079c03337a412e64c6e8a0234f
Brian Vance <briakvanc27@gmail.com>
2025-01-16 13:06:42 -0600

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,21 +40,28 @@ const keyboardMap = new Map<string, string>([
   ["M", "default"],
 ]);
 
+const defaultGuesses = [[], [], [], [], [], []];
+
 export const KeyMapContext = createContext<Map<string, string>>(keyboardMap);
-export const GuessContext = createContext<string[][]>([[], [], [], [], [], []]);
+export const GuessContext = createContext<string[][]>(defaultGuesses);
 
 export default function Home() {
   const [keyMap, setKeyMap] = useState(keyboardMap);
+  const [guesses, setGuesses] = useState(defaultGuesses);
   const [activeRow, setActiveRow] = useState(0);
   const [activeColumn, setActiveColumn] = useState(0);
 
   const map = useContext(KeyMapContext);
-  const guesses = useContext(GuessContext);
+  const guess = useContext(GuessContext);
 
   const resetKeyboardMap = () => {
     keyboardMap.forEach((value, key) => {
       keyboardMap.set(key, "default");
     });
+    guess.forEach((row) => {
+      row = [];
+    });
+    setGuesses([[], [], [], [], [], []]);
     setKeyMap(keyboardMap);
     console.log("resetting keyboard map", keyboardMap);
   };
@@ -64,28 +71,32 @@ export default function Home() {
       const key = event.key.toUpperCase();
       if (key === "BACKSPACE") {
         console.log("Backspace pressed");
+        if (activeRow < 6) guess[activeRow][activeColumn - 1] = "";
+        if (activeColumn > 0) setActiveColumn(activeColumn - 1);
       }
 
       if (activeColumn === 6) {
         console.log("Row is full");
         if (key === "ENTER") {
-          // CHECK AGAINST GUESSES AND UPDATE KEYBOARD
-          const listOfLetters = guesses[activeRow];
-          listOfLetters.forEach((letter) =>
-            setKeyMap(new Map(map.set(letter, "selected-right")))
-          );
+          // CHECK AGAINST VALID LIST AND PREVIOUS GUESSES
+          if (true) {
+            // THEN
+            // UPDATE KEYBOARD
+            const listOfLetters = guess[activeRow];
+            listOfLetters.forEach(
+              (letter) => setKeyMap(new Map(map.set(letter, "selected-right"))) // TODO update with the right color
+            );
+          }
 
           // INCREMENT POSITION
           setActiveColumn(0);
-          if (activeRow < 5) {
-            setActiveRow(activeRow + 1);
-          }
+          setActiveRow(activeRow + 1);
         }
       } else {
         if (map.get(key)) {
           if (activeRow < 6 && activeColumn < 6) {
             console.log("Key found in map", activeRow, activeColumn);
-            guesses[activeRow][activeColumn] = key;
+            guess[activeRow][activeColumn] = key;
             setActiveColumn(activeColumn + 1);
           } else {
             console.log("Guess grid is full");
@@ -103,16 +114,18 @@ export default function Home() {
   // TODO unsure about this dependency array
   return (
     <div>
-      {guesses.map((guess, index) => (
+      {guess.map((guess, index) => (
         <div>{guess.join(",")}</div>
       ))}
       <KeyMapContext.Provider value={keyMap}>
-        <GuessGrid
-          guesses={guessTest}
-          activeCol={activeColumn}
-          activeRow={activeRow}
-        ></GuessGrid>
-        <Keyboard></Keyboard>
+        <GuessContext.Provider value={guesses}>
+          <GuessGrid
+            guesses={guessTest}
+            activeCol={activeColumn}
+            activeRow={activeRow}
+          ></GuessGrid>
+          <Keyboard></Keyboard>
+        </GuessContext.Provider>
       </KeyMapContext.Provider>
       <button onClick={resetKeyboardMap}>reset</button>
     </div>
